473dae99d06eb9077797e375902916c23f7c3fa2
nandabi <valerie.nandabi@student.moringaschool.com>
2024-01-09 08:29:34 +0300

    commit message

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ const tutorials = [
 const titleCased = () => {
   return tutorials
 }
+
+
+function titleCased(first) {
+  let cap = tutorials.map((first) => first.split(" "));
+  console.log(first.split(""));
+  return first
+  .split("")
+  .map ((word) => word[0].toUpperCase() + word.substring(1))
+  .join(" ");
+};
+return cap;
+
